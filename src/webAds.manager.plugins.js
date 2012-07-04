1ffda2615e3506f85c3ddb3244ac7311bdc77d9a
Rune Maurtvedt <rune@finn.no>
2012-07-04 14:21:02 +0200

    New plugins for overlay and survey

--- src/webAds.manager.plugins.js
+++ src/webAds.manager.plugins.js
@@ -1,68 +1,91 @@
 var FINN = FINN||{};
 
 (function(F, $){
-  "use strict";
-  F.webAds = F.webAds||{};
+    "use strict";
+    F.webAds = F.webAds||{};
 
-  var plugins = {};  
-  F.webAds.registerPlugin = function(name, plug){
-    var plugin = {};
-    plugin.run = plug.run||$.noop;
-    if (typeof plug.init === 'function'){
-      plug.init(plugin);
-    }
-    plugins[name] = plugin;
-  };
-  
-  F.webAds.getPlugin = function(name){
-    return (plugins[name].run||null);
-  };
-
-  F.webAds.registerPlugin('survey', {
-	run: function(){
-		this.plugin('overlay', {url:'http://www.finn.no',title:'Survey'});
-	}
-})
-
-  /* parameters made available for third party networks */
-  F.webAds.registerPlugin('contextData', {
-	init: function(){},
-	run: function(){ return FINN.data.banner;}
-  });
-  /* overlay with iframe content, or wrap ad iframe*/
-  F.webAds.registerPlugin('overlay', {
-    init: function(plugin){},
-    run: function(options, callback){
-      var w = options.width;
-      var h = options.height;
-
-      var overlay = $('<iframe />', {
-        "class" : "iframe",
-        "src": options.url,
-        "style" : 'z-index:999999;position:fixed;width:50%;height:50%;left:30%;top:20%;background:red;'
-      }).html('<h1>'+options.title+'</h1>');
-      
-      $("body").append(overlay);
-      
-      overlay.on('click', function(e){
-        e.stopPropagation();                
-      });
-      
-      $(window).on('keydown.overlay', function(e){
-        if (e.which === 27){ 
-          overlay.remove();
-          $(window).off('keydown.overlay');
+    var plugins = {};
+    F.webAds.registerPlugin = function(name, plug){
+        var plugin = {};
+        plugin.run = plug.run||$.noop;
+        if (typeof plug.init === 'function'){
+            plug.init(plugin);
         }
-      });
-      
-      $("body").one('click', function(){
-        overlay.remove();
-      });
-      
-      overlay.focus();
-      
-    }
-  });
-  
-  
+        plugins[name] = plugin;
+    };
+
+    F.webAds.getPlugin = function(name){
+        return (plugins[name].run||null);
+    };
+
+    F.webAds.registerPlugin('survey', {
+        run: function(options, callback){
+            var daysForCookieToLive = 21;
+            var percentageToHit     = options.percent;
+            var cookieName          = options.surveyName||'qbFINN';
+            var cookie              = getCookie(cookieName);
+
+
+            function setExpDate(daysToLive) {
+                var today = new Date();
+                var expire = new Date(today.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+                expire = expire.toGMTString();
+                return expire;
+            }
+
+            function setCookie(cookieName, cookieValue, daysToLive) {
+                var cookieLife = setExpDate(daysToLive);
+                document.cookie = cookieName + "=" + cookieValue + "; expires=" + cookieLife + "; domain=.finn.no; path=/";
+            }
+
+            function getCookie(name) {
+                var currentCookie = document.cookie;
+                var index = currentCookie.indexOf(name + "=");
+                if (index == -1) return null;
+                index = currentCookie.indexOf("=", index) + 1; // first character
+                var endstr = currentCookie.indexOf(";", index);
+                if (endstr == -1) endstr = currentCookie.length; // last character
+                return unescape(currentCookie.substring(index, endstr));
+            }
+
+            function isValid() {
+                var popRandom = (100 * Math.random());
+                return  ((popRandom + percentageToHit) > 100) && !(cookie);
+            }
+
+            if (isValid()) {
+                var newCookieValue = cookie ? cookie + 1 : 1;
+                setCookie(cookieName, newCookieValue, daysForCookieToLive);
+                //display questback
+                this.plugin('overlay', options);
+            }
+
+        }
+    })
+
+    /* parameters made available for third party networks */
+    F.webAds.registerPlugin('contextData', {
+        init: function(){},
+        run: function(){ return FINN.data.banner;}
+    });
+    /* overlay with iframe content, or wrap ad iframe*/
+    F.webAds.registerPlugin('overlay', {
+        init: function(plugin){},
+        run: function(options, callback){
+            var w = options.width||'80%';
+            var h = options.height||'400px';
+            var content;
+
+
+            if (options.url){
+                content = '<iframe style="width:'+w+';height:'+h+'" src="'+options.url+'"/>';
+            } else if (options.content){
+                content = options.content;
+            }
+            FINN.user.dialog.box({ content: content });
+
+        }
+    });
+
+
 })(FINN, jQuery);
