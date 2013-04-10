e6a04ec5340e6442aacc258bcdef0c028bbacc60
Rune Maurtvedt <rune.maurtvedt@finntech.no>
2013-04-10 11:36:10 +0200

    Updated version 2.3 of the tracking script. Added score to burt variable

--- src/lib/before.rich.js
+++ src/lib/before.rich.js
@@ -1,1 +1,1 @@
-var FINN = FINN||{};FINN.data = FINN.data||{};FINN.data.banner = FINN.data.banner||{};if (FINN.data.banner.ext_logger == "true") {
+var FINN = FINN||{};FINN.data = FINN.data||{};FINN.data.banner = FINN.data.banner||{};if (FINN.data.banner.ext_logger == "true") {window.byburt_verticalSection = FINN.data.banner.score;
